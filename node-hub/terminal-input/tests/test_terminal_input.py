c2d5c621c05d63c50fdfac89ce8534d4ac85bc87
haixuanTao <tao.xavier@outlook.com>
2024-09-04 11:34:24 +0200

    Making test part of the module

--- node-hub/terminal-input/tests/test_terminal_input.py
+++ node-hub/terminal-input/tests/test_terminal_input.py
@@ -7,3 +7,8 @@ def test_import_main():
     # Check that everything is working, and catch dora Runtime Exception as we're not running in a dora dataflow.
     with pytest.raises(RuntimeError):
         main()
+
+
+if __name__ == "__main__":
+    # Call pytest.main() to run tests
+    pytest.main(["-s", "-v"])

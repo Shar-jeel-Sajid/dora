3bb01b9738de5fc82f1fb0445057c02f4665cbd8
Shar-jeel-Sajid <sharjeelsajid09@gmail.com>
2025-03-19 14:53:13 +0500

    check

--- node-hub/dora-rerun/src/series.rs
+++ node-hub/dora-rerun/src/series.rs
@@ -1,6 +1,6 @@
 use dora_node_api::{
     arrow::array::{Float32Array, Float64Array, Int32Array, Int64Array},
-    arrow::datatypes::Datatype,
+    arrow::datatypes::Datatype::{Float32, Float64, Int32, Int64},
     dora_core::config::DataId,
     ArrowData,
 };
@@ -9,8 +9,11 @@ use rerun::RecordingStream;
 
 pub fn update_series(rec: &RecordingStream, id: DataId, data: ArrowData) -> Result<()> {
     match data.data_type() {
-        DataType::Float32 | DataType::Int32 | DataType::Int64 | Datatype::Float64 => {
-            let buffer = as_primitive_array::<data.data_type>(&data);
+        Float32 => {
+            let buffer: &Float32Array = data
+                .as_any()
+                .downcast_ref()
+                .context("series is not float32")?;
             let series: Vec<_> = buffer.values().to_vec();
             for (i, value) in series.iter().enumerate() {
                 rec.log(
@@ -20,6 +23,49 @@ pub fn update_series(rec: &RecordingStream, id: DataId, data: ArrowData) -> Resu
                 .wrap_err("could not log series")?;
             }
         }
+        Float64 => {
+            let buffer: &Float64Array = data
+                .as_any()
+                .downcast_ref()
+                .context("series is not float64")?;
+            let series: Vec<_> = buffer.values().to_vec();
+            for (i, value) in series.iter().enumerate() {
+                rec.log(
+                    format!("{}_{}", id.as_str(), i),
+                    &rerun::Scalar::new(*value),
+                )
+                .wrap_err("could not log series")?;
+            }
+        }
+        Int32 => {
+            let buffer: &Int32Array = data
+                .as_any()
+                .downcast_ref()
+                .context("series is not Int32")?;
+            let series: Vec<_> = buffer.values().to_vec();
+            for (i, value) in series.iter().enumerate() {
+                rec.log(
+                    format!("{}_{}", id.as_str(), i),
+                    &rerun::Scalar::new(*value as f64),
+                )
+                .wrap_err("could not log series")?;
+            }
+        }
+        Int64 => {
+            let buffer: &Int64Array = data
+                .as_any()
+                .downcast_ref()
+                .context("series is not Int64")?;
+            let series: Vec<_> = buffer.values().to_vec();
+            for (i, value) in series.iter().enumerate() {
+                rec.log(
+                    format!("{}_{}", id.as_str(), i),
+                    &rerun::Scalar::new(*value as f64),
+                )
+                .wrap_err("could not log series")?;
+            }
+        }
+
         _ => unimplemented!("This has not yet implemented. Please contribute to dora-rerun :)"),
     }
     Ok(())
